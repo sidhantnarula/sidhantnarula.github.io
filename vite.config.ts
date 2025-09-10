11aa5404629abcdc030f8434c14613c6986e214b
sidhantnarula <sidhantnarula40@gmail.com>
2025-09-10 14:57:04 -0400

    ninth commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',   // <-- must match the repository name
   plugins: [react()],
+  base: '/', // For username.github.io repositories
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
