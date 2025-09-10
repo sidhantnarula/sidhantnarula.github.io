5aac9212e718a612c809039175a90bdd5dd28b82
sidhantnarula <sidhantnarula40@gmail.com>
2025-09-10 15:14:44 -0400

    12th commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // For username.github.io repositories
+  base: './', // Use relative paths for GitHub Pages
   build: {
     rollupOptions: {
       output: {
